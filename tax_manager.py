a33b70277dfa735789cd1355e934ddeb8a28591a
Karan Gaba <Kgaba031@gmail.com>
2021-03-20 01:00:54 -0400

    First Commit

---
+++ tax_manager.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
