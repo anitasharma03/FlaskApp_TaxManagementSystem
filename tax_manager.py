626b2c50180256645f9845ca86ce7fc56d5f7472
Karan Gaba <Kgaba031@gmail.com>
2021-03-20 14:40:48 -0400

    added static

--- tax_manager.py
+++ tax_manager.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
-
 
 @app.route('/')
 @app.route('/home')
@@ -27,32 +25,3 @@ def aboutus():
 @app.route('/contactus')
 def contactus():
     return render_template('contactus.html')
-=======
-from flask import Flask, render_template
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-
-@app.route('/aboutus')
-def aboutus():
-    return render_template('aboutus.html')
-
-
-@app.route('/contactus')
-def contactus():
-    return render_template('contactus.html')
->>>>>>> f2bf3c4c00d400c5fa236930d105602ae696b14d
