5b29239443efe9eae4818605d1f1b621f80a3ec4
Karan Gaba <Kgaba031@gmail.com>
2021-04-14 07:46:53 -0400

    login and registered working, passwords are encrypted, landing page and profile page added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
-
+from flask_bcrypt import Bcrypt
 from pymongo import MongoClient
+from flask_login import LoginManager, login_user, current_user
 client = MongoClient(
     'mongodb+srv://test:test@cluster0.fcu7b.mongodb.net/test?retryWrites=true&w=majority')
 db = client.taxmanager
@@ -8,23 +9,41 @@ accounts = db.accounts
 
 app = Flask(__name__)
 
+bcrypt = Bcrypt()
+
+
 @app.route('/')
+@app.route('/landing')
+def landing_page():
+    return render_template('landing_page.html')
+
+
 @app.route('/home')
 def home():
     return render_template('home.html')
-    
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
+login_manager = LoginManager()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
 @app.route('/check_login', methods=['GET', 'POST'])
 def check_login():
     if request.method == 'POST':
-        username = request.form['uname']
+        username = user.query.get(form.username.data)
         password = request.form['psw']
     for i in accounts.find():
-        if i.key() == username and i.value() == password:
+        if i["email"] == username and bcrypt.check_password_hash(i["password"], password):
+            login_user(username)
             return redirect("/home")
         else:
             continue
@@ -35,16 +54,18 @@ def check_login():
 def register():
     return render_template('register.html')
 
+
 @app.route('/make_account', methods=['GET', 'POST'])
 def make_account():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['psw']
-        password_repeat=request.form['psw-repeat']
+        password_repeat = request.form['psw-repeat']
         if password == password_repeat:
-            credentials={
-                "email":email,
-                "password":password
+            password = bcrypt.generate_password_hash(password).decode('utf-8')
+            credentials = {
+                "email": email,
+                "password": password
             }
             accounts.insert_one(credentials)
             return redirect("/home")
@@ -61,9 +82,15 @@ def contactus():
     return render_template('contactus.html')
 
 
-# test to insert data to the data base
-@app.route("/test")
-def test():
-    accounts.insert_one({"name": "John"})
-    return "Connected to the data base!"
+@app.route('/term')
+def term():
+    return render_template('term.html')
 
+
+@app.route('/form')
+def form():
+    return render_template('tax_form.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
