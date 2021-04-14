1f787887e202ca0bef58ac9bb0c44055986d9563
Karan Gaba <Kgaba031@gmail.com>
2021-04-14 04:16:47 -0400

    deleted db.py

--- db.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from pymongo import MongoClient
-client = MongoClient(
-    'mongodb+srv://KaranGaba:<KaranGaba>@cluster0.fcu7b.mongodb.net/tax_manager?retryWrites=true&w=majority')
-db = client.taxmanager
-accounts = db.accounts
